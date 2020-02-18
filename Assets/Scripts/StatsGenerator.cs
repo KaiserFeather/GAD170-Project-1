fa0227c9539a076eea2e51960f607f4a21314501
KaiserFeather <jconroy.f98@gmail.com>
2020-02-18 18:59:05 +0400

    Added project 1
    XP system done, level system done, XP requirements needs to be altered, stat changes are done, battle calculator is done

---
+++ Assets/Scripts/StatsGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  A <see langword="static"/> class with methods (functions) for initialising or randomising the stats class.
+///  
+/// TODO:
+///     Initialise a stats instance with generated starting stats
+///     Handle the assignment of extra points or xp into an existing stats of a character
+///         - this is expected to be used by NPCs leveling up to match the player.
+/// </summary>
+public static class StatsGenerator
+{
+    public static void InitialStats(Stats stats)
+    {
+        stats.level = 1;
+
+        stats.xp = 0;
+
+        int totalPoints = 10;
+        stats.rhythm = Random.Range(1, totalPoints);
+        totalPoints -= stats.rhythm;
+
+        stats.style = Random.Range(1, totalPoints);
+        totalPoints -= stats.style;
+
+        stats.luck = totalPoints;
+    }
+
+    public static void AssignUnusedPoints(Stats stats, int points)
+    {
+    }
+}
---
+++ Assets/Scripts/XPHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is responsible for converting a battle result into xp to be awarded to the player.
+/// 
+/// TODO:
+///     Respond to battle outcome with xp calculation based on;
+///         player win 
+///         how strong the win was
+///         stats/levels of the dancers involved
+///     Award the calculated XP to the player stats
+///     Raise the player level up event if needed
+/// </summary>
+public class XPHandler : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GameEvents.OnBattleConclude += GainXP;
+    }
+
+    private void OnDisable()
+    {
+    }
+
+    public void GainXP(BattleResultEventData data)
+    {
+        if (data.outcome > 0)
+        {
+            data.player.xp = 100 * (int)data.outcome;
+            GameEvents.PlayerXPGain(data.player.xp);
+        }
+ 
+        int XPrequired = 1000 + (data.player.level * 100);
+
+        if (data.player.xp >= XPrequired)
+		{
+            data.player.level += 1;
+            GameEvents.PlayerLevelUp(data.player.level);
+            data.player.xp = 0;
+		}
+    }
+}
